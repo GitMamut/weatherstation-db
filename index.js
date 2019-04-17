6881ac58f3a4e3e163f677f3c35f6720ded2ab5c
Przemek <vatier@gazeta.pl>
2019-04-17 22:27:01 +0200

    get last value from db

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express')
 const firebase = require("firebase");
 const config = require("./config");
 
-console.log("Initializing Firebase DB...");
+console.log("Initializing DB...");
 firebase.initializeApp(config.db);
-console.log("Firebase DB started");
+console.log("DB started");
 
 
 const server = express();
 server.get('/', (req, res) => {
-    console.log("DB > ref('/hello/')");
+    console.log("DB >");
 
-    firebase.database().ref('/hello/').once('value')
-    .then(snapshot => res.send(snapshot.val()))
+    firebase.database().ref('/sensor-readings/').orderByKey().limitToLast(1).once('value')
+    .then(snapshot => {
+        const dateKey = Object.keys(snapshot.val())[0]
+        const values = snapshot.val()[dateKey]
+        res.send(values)
+    })
     .catch(e => console.log(e));
 })
 
