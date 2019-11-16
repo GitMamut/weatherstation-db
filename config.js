74272db00d072874e487b002b10a802b0a0ba90a
Przemek <vatier@gazeta.pl>
2019-11-16 22:04:39 +0100

    add pm for lametric

--- config.js
+++ config.js
@@ -7,3 +7,9 @@ exports.db = {
 };
 
 exports.PORT = process.env.PORT || 5000
+
+exports.pm10_icons = ["i18018", "i18019", "i18020", "i18021", "i18022"];
+exports.pm10_scale = 50.0;
+
+exports.pm2_5_icons = ["i18023", "i18024", "i18025", "i18026", "i18027"];
+exports.pm2_5_scale = 25.0;
--- index.js
+++ index.js
@@ -27,6 +27,35 @@ server.get('/', (req, res) => {
 })
 
 
+server.get('/lametric/pm/now', (req, res) => {
+    console.log("DB >");
+
+    firebase.database().ref('/sensor-readings/').orderByKey().limitToLast(1).once('value')
+    .then(snapshot => {
+        const dateKey = Object.keys(snapshot.val())[0]
+        const values = snapshot.val()[dateKey]
+        const pm10 = values.espeasy_PM10
+        const pm10_iconIndex = Math.min(parseInt(pm10 / config.pm10_scale * 4.0), 4)
+        const pm2_5 = values.espeasy_PM2_5
+        const pm2_5_iconIndex = Math.min(parseInt(pm2_5 / config.pm2_5_scale * 4.0), 4)
+        res.send({
+            frames: [
+                {
+                    text: pm10,
+                    icon: config.pm10_icons[pm10_iconIndex],
+                    index: 0
+                },
+                {
+                    text: pm2_5,
+                    icon: config.pm2_5_icons[pm2_5_iconIndex],
+                    index: 1
+                }
+            ]
+        })
+    })
+    .catch(e => console.log(e));
+})
+
 server.get('/lametric/out/now', (req, res) => {
     console.log("DB >");
 
